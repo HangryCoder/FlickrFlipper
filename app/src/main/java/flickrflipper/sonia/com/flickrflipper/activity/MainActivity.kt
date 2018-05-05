3c22daa601a2ce7306a8c184bee5035ea44aa6c1
Sonia Wadji <soniawadji@gmail.com>
2018-05-05 16:16:10 +0530

    Commit "1.2"
    Created Dummy GridLayout with Images..
    Need to feed Flickr Images

--- app/src/main/java/flickrflipper/sonia/com/flickrflipper/activity/MainActivity.kt
+++ app/src/main/java/flickrflipper/sonia/com/flickrflipper/activity/MainActivity.kt
@@ -2,12 +2,46 @@ package flickrflipper.sonia.com.flickrflipper.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
 import flickrflipper.sonia.com.flickrflipper.R
+import flickrflipper.sonia.com.flickrflipper.adapter.FlickrAdapter
+import flickrflipper.sonia.com.flickrflipper.model.FlickrPhoto
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var flickrPhotosList: ArrayList<FlickrPhoto> = ArrayList()
+    private lateinit var flickrAdapter: FlickrAdapter
+    private lateinit var gridlayoutManager: GridLayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        flickrPhotosList.clear()
+
+        addPhotos()
+
+        flickrAdapter = FlickrAdapter(this, flickrPhotosList)
+        gridlayoutManager = GridLayoutManager(this, 2)
+        recyclerView.layoutManager = gridlayoutManager
+        recyclerView.adapter = flickrAdapter
+    }
+
+    private fun addPhotos() {
+        var flickrPhoto = FlickrPhoto(1, "Sonia", "adcsd")
+        flickrPhotosList.add(flickrPhoto)
+
+        flickrPhoto = FlickrPhoto(1, "Shilpa", "adcsd")
+        flickrPhotosList.add(flickrPhoto)
+
+        flickrPhoto = FlickrPhoto(1, "Mommy", "adcsd")
+        flickrPhotosList.add(flickrPhoto)
+
+        flickrPhoto = FlickrPhoto(1, "Daddy", "adcsd")
+        flickrPhotosList.add(flickrPhoto)
+
+        flickrPhoto = FlickrPhoto(1, "Stephen", "adcsd")
+        flickrPhotosList.add(flickrPhoto)
     }
 }
